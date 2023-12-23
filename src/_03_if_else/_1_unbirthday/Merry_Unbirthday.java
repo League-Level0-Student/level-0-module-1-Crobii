fdfe44b8a5afd455c10325ffb5db8c1ad2e1871d
Crobii <149904273+Crobii@users.noreply.github.com>
2023-12-23 14:28:51 -0800

    unbirthday

---
+++ src/_03_if_else/_1_unbirthday/Merry_Unbirthday.java
@@ -0,0 +1,15 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Merry_Unbirthday {
+	public static void main(String[] args) {
+		String birthday = JOptionPane.showInputDialog(null, "What day is your birthday?");
+	if (birthday.equals ("9/20")) {
+	JOptionPane.showMessageDialog(null, "happy birthday!");
+	else {
+		JOptionPane.showMessageDialog(null, "merry unbirthday!")
+	
+	}
+	}
+}
